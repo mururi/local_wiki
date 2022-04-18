f7e5c3474f0883c1f202c5a54a8cc0c6c4682155
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-18 19:21:20 +0300

    Create registration form and register view

--- wiki/views.py
+++ wiki/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrerForm
+from django.contrib.auth import login
 
 def home(request):
     return render(request, 'wiki/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrerForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrerForm
+    
+    return render(request, 'registration/register.html', {"form": form})
