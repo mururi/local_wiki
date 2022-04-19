9c52152b24dc66ea1d38509084307fbca3c18a5c
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-20 00:48:47 +0300

    Setup business model test class

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from . models import NeighborHood
+from . models import NeighborHood, Business
 
 
 class NeighborhoodTestClass(TestCase):
@@ -43,3 +43,26 @@ class NeighborhoodTestClass(TestCase):
 
         neighborhood = NeighborHood.find_neigborhood(1)
         self.assertEqual(neighborhood.name, 'test neighborhood')
+
+class BusinessTestClass(TestCase):
+    '''
+     Test class to test the behaviour of the Business class and methods
+    '''
+
+    def setUp(self):
+        '''
+        Setup method to run before each test case
+        '''
+        # Creating a new user
+        self.new_user = User(username = 'Doe', email = 'johndoe@company.com', password = 'test_pass')
+        self.new_user.save()
+
+        # Creating a new neighborhood
+        self.new_neighborhood = NeighborHood(id = 1, name = 'test neighborhood', location = 'test location', occupants = 10, admin = self.new_user)
+        self.new_neighborhood.save()
+
+        # Creating a new business
+        self.new_business = Business(id = 1, name = 'test business', description = 'test business description', email = 'info@business.com', user = self.new_user, neighborhood = self.new_neighborhood)
+        self.new_business.save()
+
+        
