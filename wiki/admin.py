ba592434695ab44d5da82c50e23cc6ff51dcf4dc
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-19 22:23:49 +0300

    Register models on admin site

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, NeighborHood, Post
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(NeighborHood)
+admin.site.register(Post)
