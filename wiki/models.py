626784e199b590ff8280d177a9665f263e8ed47e
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-19 13:26:13 +0300

    Create business model and modify neighborhood model

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,4 @@
+import email
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
@@ -9,11 +10,28 @@ class NeighborHood(models.Model):
     occupants = models.IntegerField()
     admin = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
 
 class Profile(models.Model):
     profile_pic = CloudinaryField('image')
     name = models.CharField(max_length=50)
     bio = models.TextField()
+    location = models.CharField(max_length=50)
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     neighborhood = models.ForeignKey(NeighborHood, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Business(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    email = models.EmailField(max_length=60)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(NeighborHood, null=True, on_delete=True)
+
+    def __str__(self):
+        return self.name
 
